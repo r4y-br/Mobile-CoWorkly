7a9736eeaea15a425389a4fe65d7a4d9035c7cf4
0xGr1tL1 <laith.gritli@supcom.tn>
2025-12-30 22:28:28 +0100

    removed test file and duplicated functions

--- server/src/routes/auth.js
+++ server/src/routes/auth.js
@@ -5,7 +5,6 @@ import {
     refreshToken, 
     logout, 
     getProfile,
-    getMyProfile,
     updateProfile 
 } from "../controllers/auth.js";
 import { authenticate } from "../middlewares/auth.js";
@@ -19,8 +18,7 @@ router.post('/refresh', refreshToken);
 
 // these are protected routes
 router.post('/logout', authenticate, logout);
-router.get('/profile', authenticate, getProfile);
-router.get('/me', authenticate, getMyProfile);
+router.get('/me', authenticate, getProfile);
 router.put('/profile', authenticate, updateProfile);
 
 export default router;
